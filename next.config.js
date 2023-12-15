c0357030a1053c6f55935fd5e53e9d701ae38a1c
Unboxx Business - Start, Grow, and Scale Your Online Business <121428085+unboxxbusiness@users.noreply.github.com>
2023-12-15 19:15:51 +0530

    third commit

--- next.config.js
+++ next.config.js
@@ -17,12 +17,12 @@ const nextConfig = {
         domains: ["s.gravatar.com"]
     },
     env: {
-        siteTitle: "Your Company",
-        siteDescription: "Your company description.",
-        siteKeywords: "your company keywords",
-        siteUrl: "You site url",
+        siteTitle: "unboxxbusiness",
+        siteDescription: "Join the challenge now for only 5k (or get free access with your NextSalesman AI trial)",
+        siteKeywords: "wordpress tutorial, blogging, wordpress free course, wordpress affiliate marketing course, wordpress in hindi, wordpress hindi course, web design institue in noida, web design institute in delhi, web design course free, wordpress web design course",
+        siteUrl: "https://unboxxbusiness.com",
         siteImagePreviewUrl: "/images/preview.jpeg",
-        twitterHandle: "@your_handle"
+        twitterHandle: "@unboxxbusiness"
     },
     headers() {
         return [
